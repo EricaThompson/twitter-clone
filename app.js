7c93cb3333fb3c66df7f624f6e8ed9e7ce01b879
ericathompson <ericathompson.nc@gmail.com>
2020-12-19 21:02:04 -0800

    Add users and tweets api routes

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const express = require("express");
 const app = express();
 
 const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const bodyParser = require('body-parser');
+
+
 mongoose
-  .connect(db, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+.connect(db, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+    
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 app.get("/", (req, res) => res.send("Hello to all"));
 
